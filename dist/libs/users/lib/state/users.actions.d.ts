739fa4f20a05e0233e77288a96a81662f425a0d7
adelberhan <adel.berha@gmail.com>
2026-03-26 09:42:03 +0200

    remove dist

--- dist/libs/users/lib/state/users.actions.d.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '../modules/user';
-export declare const buildUserSession: import("@ngrx/store").ActionCreator<"[Users] Build User Session", () => import("@ngrx/store/src/models").TypedAction<"[Users] Build User Session">>;
-export declare const buildUserSessionSuccess: import("@ngrx/store").ActionCreator<"[Users] Load Users Success", (props: {
-    user: User;
-}) => {
-    user: User;
-} & import("@ngrx/store/src/models").TypedAction<"[Users] Load Users Success">>;
-export declare const buildUserSessionFailure: import("@ngrx/store").ActionCreator<"[Users] Build User Session Failed", () => import("@ngrx/store/src/models").TypedAction<"[Users] Build User Session Failed">>;
--- dist/libs/users/lib/state/users.selectors.d.ts
+++
@@ -1,4 +0,0 @@
-import { UsersPartialState, usersState } from './users.reducer';
-export declare const getUsersState: import("@ngrx/store").MemoizedSelector<UsersPartialState, usersState, import("@ngrx/store").DefaultProjectorFn<usersState>>;
-export declare const getUser: import("@ngrx/store").MemoizedSelector<UsersPartialState, import("@ng-shop/users").User, (s1: usersState) => import("@ng-shop/users").User>;
-export declare const getUserIsAuth: import("@ngrx/store").MemoizedSelector<UsersPartialState, boolean, (s1: usersState) => boolean>;
